d57b4a3a66481170b5016f86f1e1407945cb2803
dtalochk <denis.talochkin@gmail.com>
2021-11-12 00:40:47 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure(awsExports);
+
+const AppWithAuth = withAuthenticator(App);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {window.location.hostname === 'localhost' ? <App/> : <AppWithAuth />}
   </React.StrictMode>,
   document.getElementById('root')
 );
